ed73d14b42e68ef086a3d8ccc4b059b842f50036
cem tugut <cem.tugut@hotmail.com.tr>
2022-01-12 10:37:48 +0300

    web pages (success,fail,status)

--- src/Goodnites.Payment.Payeer.Web/Pages/Payeer/Success.cshtml.cs
+++ src/Goodnites.Payment.Payeer.Web/Pages/Payeer/Success.cshtml.cs
@@ -1,12 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Goodnites.Payment.Payeer.Web.Pages.Payeer
 {
     public class Success : PageModel
     {
-        public void OnGet()
+        private readonly IPayeerApiAppService _payeerApiAppService;
+
+        public Success(IPayeerApiAppService payeerApiAppService)
         {
-            
+            _payeerApiAppService = payeerApiAppService;
         }
+
+        public async Task OnGetAsync(PayeerResponseModel model)
+        {
+            var clientIp = HttpContext.Connection.RemoteIpAddress.ToString();
+
+            await _payeerApiAppService.WebHookAsync(model, clientIp);
+        }
+        
+        
     }
 }
